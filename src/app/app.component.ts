91d90b3dbbff7bb325967acf1fe4157a2f164f51
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-24 12:58:39 +0100

    Created a method for adding a new server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,4 +45,12 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: ' New Server',
+      status: 'stable',
+      started: new Date(24, 8, 2019)
+    });
+  }
 }
